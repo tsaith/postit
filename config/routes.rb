08126738c4af408a13c424ed197d64af98d3bc67
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2014-11-12 18:33:54 +0800

    trace the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
- 
+
   resources :posts, except: [:destroy] do
-    resources :comments, only: [:new, :create] 
+    member do
+      post :vote
+    end
+
+    resources :comments, only: [:create] do
+      member do
+        post :vote
+      end
+    end
   end
+
+  resources :categories, except: [:destroy]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/register', to: 'users#new'
+
+  resources :users, only: [:show, :create, :edit, :update]
+
 end
