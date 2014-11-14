2b0cbe0fe8564ba9d94109a6d7920f69f7f56f16
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2014-11-14 09:57:58 +0800

    fix a bug in the model Vote.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
       redirect_to post_path(@post)
     else
       render 'posts/show'
-    end  
+    end
 
   end
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
 
-  validates_uniqueness_of :user, scope: :voteable
+  validates_uniqueness_of :user, scope: [:voteable_type, :voteable_id]
 end
